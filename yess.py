43dc9499260a5710f5171ccad6e244eecc58a33c
Carlmwa <114201533+Carlmwa@users.noreply.github.com>
2023-03-19 18:21:44 +0800

    Add files via upload

---
+++ yess.py
@@ -0,0 +1,8 @@
+import webbrowser
+import time
+
+# Wait for 5 seconds
+time.sleep(5)
+
+# Open the YouTube video for "Never Gonna Give You Up"
+webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
